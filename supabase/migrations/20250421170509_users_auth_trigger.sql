882a08c75af73f683e80ef87ab0ebe1b6977ac84
Hunter White <hunterwhite.dev@gmail.com>
2025-04-22 12:12:47 -0400

    added auth trigger for when users signup

---
+++ supabase/migrations/20250421170509_users_auth_trigger.sql
@@ -0,0 +1,18 @@
+
+-- inserts a row into public.profiles
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  insert into public.users (user_id, username, email)
+  values (new.id, new.raw_user_meta_data ->> 'username', new.email);
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
