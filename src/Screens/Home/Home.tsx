ed8f6ae0a5b106a593b0e45cc8b8104f10dc93c9
Hunter White <hunterwhite.dev@gmail.com>
2025-04-25 13:29:30 -0400

    removed useFunction from Home.tsx

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -3,10 +3,18 @@ import { Sidebar } from "../../Components/Sidebar";
 import Post from "../../Components/PostCard/PostCard";
 import { Stories } from "../../Components/Stories";
 import useFunction from "../../hooks/useFunction";
+import supabase from "../../supabase";
 function Home() {
-  // const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState();
 
-  const [loading, data] = useFunction("home");
+  useEffect(() => {
+    const run = async () => {
+      const { data, error } = await supabase.from("posts").select("*");
+      setPosts(data);
+    };
+
+    run();
+  }, []);
 
   return (
     <>
@@ -14,7 +22,7 @@ function Home() {
       <div className="ml-[17.5vw] pt-[2.5vh] w-3/4">
         <Stories />
         <div className="w-[500px] mx-[auto] my-[0] mt-4">
-          {data?.posts?.map((post) => <Post {...post} />)}
+          {posts?.map((post) => <Post {...post} />)}
         </div>
       </div>
     </>
