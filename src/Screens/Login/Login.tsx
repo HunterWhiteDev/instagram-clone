20cc8eb041216992b74a662834d5713465b869f5
Hunter White <hunterwhite.dev@gmail.com>
2025-04-25 11:27:21 -0400

    fixed styling on login page

--- src/Screens/Login/Login.tsx
+++ src/Screens/Login/Login.tsx
@@ -25,13 +25,16 @@ function Login() {
   return (
     <div className="flex justify-center gap-[50px] mt-[5vh]">
       <div className="login__left">
-        <img src={MainImage} />
+        <img src={MainImage} className="object-cover w-[500px]" />
       </div>
       <div className="w-[20vw]">
         <div className="flex flex-col">
-          <img src="https://i.pinimg.com/originals/57/6c/dd/576cdd470fdc0b88f4ca0207d2b471d5.png" />
+          <img
+            src="https://www.pngkey.com/png/full/1-13459_instagram-font-logo-white-png-instagram-white-text.png"
+            className="object-contain w-[250px] mx-auto my-6"
+          />
 
-          <form onSubmit={handleLogin}>
+          <form onSubmit={handleLogin} className="flex flex-col justify-center">
             <input
               className="input"
               placeholder="email"
@@ -51,18 +54,25 @@ function Login() {
               Log in
             </button>
           </form>
-          <div className="flex items-center mt-4">
-            <hr /> <span>OR</span> <hr />
+          <div className="flex items-center mt-4 justify-center [&>hr]:bg-gray-500 [&>hr]:w-full [&>hr]:mx-2">
+            <hr />
+            <span>OR</span>
+            <hr />
           </div>
 
-          <p className="mx-[auto] my-[0] text-[lightblue] mt-4 text-[14px] cursor-pointer">
+          <p className="mx-[auto] my-[0] text-[lightblue] mt-4 text-[16px] cursor-pointer">
             Forgot Password?
           </p>
         </div>
-        <div className="border-[1px] border-solid border-[gray] flex justify-center mt-2">
+        <div className="border-[1px] border-solid border-[gray] flex justify-center mt-2 rounded-md p-1">
           <p>
             Don't have an account?{" "}
-            <span onClick={() => navigate("/signup")}>Sign up</span>
+            <span
+              className="text-[lightblue] cursor-pointer"
+              onClick={() => navigate("/signup")}
+            >
+              Sign up
+            </span>
           </p>
         </div>
       </div>
