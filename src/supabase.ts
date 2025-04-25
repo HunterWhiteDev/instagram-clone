051668ff8018eb7c2dff3f2cf68efcb4c6f90a4f
Hunter White <hunterwhite.dev@gmail.com>
2025-04-25 13:28:55 -0400

    removed comment from src/supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,9 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/supabase.ts";
-// const supabase = createClient(
-//   "https://rnypbaefekpaolxsxhqo.supabase.co",
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJueXBiYWVmZWtwYW9seHN4aHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4Mjg4NjUsImV4cCI6MjAyMDQwNDg2NX0.kaoZZicWSa8TqFP-lalk_iisfaVdCNNOd90UxCJHucE"
-// );
 
 const supabase = createClient<Database>(
   "http://127.0.0.1:54321/",
